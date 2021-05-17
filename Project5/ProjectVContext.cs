a9a82e0bad4635bb5a0c425bbab9c55d20dee78a
volta76 <64655109+volta76@users.noreply.github.com>
2021-05-17 11:11:52 +0200

    Add DbContext / ConnectionString

---
+++ Project5/ProjectVContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project5.Entities;
+using Project5.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project5
+{
+    public class ProjectVContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public ProjectVContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+}
