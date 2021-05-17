304e5b88d62481e55b60b7f147ce546745c5a716
DotEmms <emma_danckaert@live.be>
2021-05-17 11:36:56 +0200

    created Register, login and Account service

--- Angular/Project5-Angular/src/app/app.module.ts
+++ Angular/Project5-Angular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,10 @@ import { PostsComponent } from './posts/posts.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/Project5-Angular/src/app/register/register.component.ts
+++ Angular/Project5-Angular/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerUser: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+  });
+  validationErrors: string[] = [];
+  
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
+  register(){
+    console.log(this.registerUser.value);
+    this.accountService.register(this.registerUser.value).subscribe(response =>{
+
+    }, error => {
+      console.log(error);
+      this.validationErrors.push(error.errors).toString;
+    });
+  }
 }
