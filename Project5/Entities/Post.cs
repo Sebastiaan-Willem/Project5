17c4a22fc3370eac33ea40c79e0f6eacd4ca8f0c
volta76 <64655109+volta76@users.noreply.github.com>
2021-05-17 10:59:52 +0200

    Add Entities / Enum

---
+++ Project5/Entities/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project5.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastModified { get; set; } = DateTime.Now;
+        public bool IsNSFW { get; set; }
+
+    }
+}
---
+++ Project5/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Project5.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project5.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProfilePicture { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastModified { get; set; } = DateTime.Now;
+        public bool IsVerified { get; set; }
+        public ICollection<Languages> Languages { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
