4466e81de5ce708ab046bb4a3161c901ef0139c8
DotEmms <emmadanckaert@hotmail.com>
2021-05-17 13:59:26 +0200

    creation of all assets

--- Angular/Project5-Angular/src/app/user.service.ts
+++ Angular/Project5-Angular/src/app/user.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private usersUrl = 'https://localhost:44302/api/users';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    const users = this.http.get<User[]>(this.usersUrl);
+    return users;
+  };
+
+  getUser(id: number): Observable<User> {
+    const url = `${this.usersUrl}/${id}`;
+    const user = this.http.get<User>(url);
+    return user;
+  };
+
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.usersUrl, user, this.httpOptions);
+  };
+
+  updateUser(user?: User): Observable<any> {
+    return this.http.put(this.usersUrl, user, this.httpOptions);
+  };
+
+  deleteUser(id: number): Observable<User> {
+    const url = `${this.usersUrl}/${id}`;
+    return this.http.delete<User>(url, this.httpOptions);
+  };
 }
--- Angular/Project5-Angular/src/app/users/users.component.ts
+++ Angular/Project5-Angular/src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[] = [];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(x => this.users = x);
+  }
+
+  addUser(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.userService.addUser({ name } as User).subscribe(x => this.users.push(x));
+  }
+
+  deleteUser(user: User): void {
+    this.users = this.users.filter(x => x !== user);
+    this.userService.deleteUser(user.id).subscribe();
   }
 
 }
