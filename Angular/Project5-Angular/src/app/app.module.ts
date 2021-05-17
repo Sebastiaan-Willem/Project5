5685a5e23dd4e92923b22e26a2ae7100202f2d3b
DotEmms <emma_danckaert@live.be>
2021-05-17 10:49:08 +0200

    added components

--- Angular/Project5-Angular/src/app/app.module.ts
+++ Angular/Project5-Angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { UsersComponent } from './users/users.component';
+import { RegisterComponent } from './register/register.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    UsersComponent,
+    RegisterComponent,
+    UserDetailComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
