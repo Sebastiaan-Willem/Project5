e188da861648bb9364f857b6e2d5cbf10a5ed58a
volta76 <64655109+volta76@users.noreply.github.com>
2021-05-17 12:11:35 +0200

    Added UserController

---
+++ Project5/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project5.Entities;
+using Project5.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            var users = await _service.GetUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _service.GetUserAsync(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddUser(User user)
+        {
+            await _service.AddUserAsync(user);
+            return Created("", null);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            await _service.DeleteUserAsync(id);
+            return Ok("User deleted");
+            //let's try it
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(User user)
+        {
+            await _service.UpdateUserAsync(user);
+            return Ok("User is up to date"); 
+        }
+    }
+}
--- Project5/Entities/User.cs
+++ Project5/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using Project5.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,12 @@ namespace Project5.Entities
     public class User
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public string Name { get; set; }
         public string ProfilePicture { get; set; }
+        [MaxLength(50)]
         public string City { get; set; }
+        [MaxLength(50)]
         public string Country { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
